9e09677ed6a0d8dae8ae6a16ac962e189c5a345f
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-10-30 13:21:39 -0300

    revisao 10

---
+++ js/view-models.js
@@ -0,0 +1,42 @@
+
+
+const card1 = document.getElementById("card-1");
+const card2 = document.getElementById("card-2");
+const card3 = document.getElementById("card-3");
+
+
+const view1= document.getElementById("view1");
+const view2= document.getElementById("view2");
+const view3= document.getElementById("view3");
+
+card1 && card1.addEventListener("click",()=>{
+     view1.style.display = 'flex';
+})
+card2 && card2.addEventListener("click",()=>{
+     view2.style.display='flex';
+})
+card3 && card3.addEventListener("click",()=>{
+     view3.style.display = 'flex';
+})
+
+const close1= document.querySelector('.close1')
+const close2= document.querySelector('.close2')
+const close3= document.querySelector('.close3')
+
+
+
+close1 && close1.addEventListener("click",()=>{
+    view1.style.display = 'none';
+
+})
+close2 && close2.addEventListener("click",()=>{
+    view2.style.display = 'none';
+    
+})
+close3 && close3.addEventListener("click",()=>{
+    view3.style.display = 'none';
+})
+
+
+
+
